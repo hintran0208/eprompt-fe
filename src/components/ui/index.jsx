fd61b1944bb6072bff3209ad858bf811c21d9dc5
Khoa - Nguyen Minh <nguyenminhkhoa.nmk98@gmail.com>
2025-07-14 23:39:01 +0700

    fix import error

--- src/components/ui/index.jsx
+++ src/components/ui/index.jsx
@@ -1,5 +1,6 @@
 export { default as Button } from "./Button";
 export { default as Card } from "./Card";
 export { default as Input } from "./Input";
+export { default as Textarea } from "./Textarea";
 export { default as ToastProvider } from "./Toast/Toast";
 export { default as ToastContext } from "./Toast/ToastContext";
